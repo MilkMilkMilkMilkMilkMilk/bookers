7381b378a90a29d08dc8ff5615e4990b2eefe0b9
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2021-11-18 13:38:12 +0000

    [Remove] viewファイル_destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,18 @@ class BooksController < ApplicationController
   def new
   end
   
+  def create
+  end
+  
   def show
   end
   
   def edit
   end
   
+  def update
+    
+  
   def destroy
+  end
 end
