5b9164aa66c1fd34a2b129e895729d5a6ee11015
MilkMilkMilkMilkMilkMilk <izumilk0922@gmail.com>
2021-11-23 08:05:39 +0000

    [ADD] bookインスタンス

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,20 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
+    @book = Book.new
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
-    
-  
+  end
+
   def destroy
   end
 end
